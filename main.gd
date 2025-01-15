455de5cc48e3270fb15a5095279ca7260ecb0273
Purple <connwann84@gmail.com>
2025-01-14 23:36:59 -0800

    Add files via upload

---
+++ main.gd
@@ -0,0 +1,252 @@
+extends Node
+
+
+onready var _Spawn = preload("res://mods/PurplePuppy-Your_Eternal_Reward/reward.gd").new()
+
+var KeybindsAPI = null
+var debug = true
+var key_states = {}
+var in_game = false
+var delay = null
+
+signal _reward
+
+var prefix = "[Reward Config Handler] "
+
+var config_data = {} 
+
+var default_config_data = {
+	"reward": 92, 
+	"new_skin_delay_seconds": 1.5, 
+}
+
+func _ready():
+	KeybindsAPI = get_node_or_null("/root/BlueberryWolfiAPIs/KeybindsAPI")
+	load_or_create_config()
+	print("[Reward] Main script is loaded successfully!")
+	add_to_group("rewardkey")
+	check_updates()
+	if KeybindsAPI:
+		var reward_signal = KeybindsAPI.register_keybind({
+			"action_name": "reward", 
+			"title": "Steal Skin", 
+			"key": config_data["reward"], 
+		})
+
+		KeybindsAPI.connect(reward_signal, self, "_reward")
+		KeybindsAPI.connect("_keybind_changed", self, "_on_keybind_changed")
+	else:
+		print("[Stamps] KeybindsAPI not found! Falling back to direct key listening.")
+		reset_config()
+	delay = config_data["new_skin_delay_seconds"]
+	add_child(_Spawn)
+	
+
+func check_key_presses():
+		
+	if Input.is_key_pressed(config_data["reward"]):
+		handle_reward()
+	else:
+		key_states["reward"] = false
+
+
+func handle_reward():
+	if not key_states.has("reward"):
+		key_states["reward"] = false
+
+	if not key_states["reward"]:
+		key_states["reward"] = true
+		_reward()
+
+
+func _get_gdweave_dir()->String:
+	if debug:
+		return "C:/Users/puppy/Desktop/wuohe"
+	else:
+		var game_directory: = OS.get_executable_path().get_base_dir()
+		var folder_override: String
+		var final_directory: String
+		for argument in OS.get_cmdline_args():
+			if argument.begins_with("--gdweave-folder-override="):
+				folder_override = argument.trim_prefix("--gdweave-folder-override=").replace("\\", "/")
+		if folder_override:
+			var relative_path: = game_directory.plus_file(folder_override)
+			var is_relative: = not ":" in relative_path and Directory.new().file_exists(relative_path)
+			final_directory = relative_path if is_relative else folder_override
+		else:
+			final_directory = game_directory.plus_file("GDWeave")
+		return final_directory
+		
+	
+func _get_config_location()->String:
+	var gdweave_dir = _get_gdweave_dir()
+	var config_path = gdweave_dir.plus_file("configs").plus_file("PurplePuppy.Your_Eternal_Reward.json")
+	return config_path
+	
+func _get_config_dir()->String:
+	var gdweave_dir = _get_gdweave_dir()
+	var config_path = gdweave_dir.plus_file("configs")
+	return config_path
+
+
+
+func load_or_create_config():
+	var config_path = _get_config_location()
+	var dir = Directory.new()
+	
+	
+	var config_dir = _get_config_dir()
+	if not dir.dir_exists(config_dir):
+		if dir.make_dir_recursive(config_dir) == OK:
+			print(prefix, "Created config directory at: ", config_dir)
+		else:
+			print(prefix, "Failed to create config directory at: ", config_dir)
+			return 
+	
+	var file = File.new()
+	
+	if file.file_exists(config_path):
+		
+		if file.open(config_path, File.READ) == OK:
+			var data = file.get_as_text()
+			file.close()
+			
+			
+			var json_result = JSON.parse(data)
+			if json_result.error == OK and typeof(json_result.result) == TYPE_DICTIONARY:
+				config_data = json_result.result
+				print(prefix, "Config loaded successfully: ", config_data)
+				if config_data.size() != 2:
+					print(prefix, "Invalid Config Size, Resetting")
+					reset_config()
+				return 
+			else:
+				print(prefix, "Failed to parse config file. Using default config.")
+				config_data = default_config_data.duplicate()
+		else:
+			print(prefix, "Failed to open config file for reading. Using default config.")
+			config_data = default_config_data.duplicate()
+	else:
+		
+		config_data = default_config_data.duplicate()
+		print(prefix, "Config file created with default values at: ", config_path)
+	
+	save_config()
+
+func reset_config():
+	var config_path = _get_config_location()
+	var file = File.new()
+	
+	
+	config_data = default_config_data.duplicate()
+	
+	
+	if file.open(config_path, File.WRITE) == OK:
+		var json_string = JSON.print(config_data, "	")
+		file.store_string(json_string)
+		file.close()
+		print(prefix, "Configuration reset to default values.")
+	else:
+		print(prefix, "Failed to open config file for writing during reset.")
+		
+
+
+func save_config():
+	var config_path = _get_config_location()
+	var file = File.new()
+
+	
+	if file.open(config_path, File.WRITE) == OK:
+		
+		var json_string = JSON.print(config_data, "	")
+		file.store_string(json_string)
+		file.close()
+		print(prefix, "Config saved successfully to: ", config_path)
+	else:
+		print(prefix, "Failed to open config file for writing: ", config_path)
+
+
+func get_action_scancode(action_name: String)->int:
+	if config_data.has(action_name):
+		return config_data[action_name]
+	else:
+		print(prefix, "Action name not found: ", action_name)
+		return - 1
+
+func check_updates():
+	var was_player_present = false
+	var config_path = _get_config_location()
+	var file = File.new()
+
+	while true:
+		yield (get_tree().create_timer(2.0), "timeout")
+		
+		var current_scene = get_tree().current_scene
+		if current_scene == null:
+			print(prefix, "No current scene found.")
+			continue
+
+		var _Player = current_scene.get_node_or_null("Viewport/main/entities/player")
+		if _Player == null:
+			in_game = false
+			if was_player_present:
+				if _Spawn:
+					_Spawn.queue_free()
+				_Spawn = preload("res://mods/PurplePuppy-Your_Eternal_Reward/reward.gd").new()
+				add_child(_Spawn)
+				print(prefix, "Player was removed. Respawned _Spawn node.")
+			was_player_present = false
+		else:
+			if not was_player_present:
+				send_keybind()
+			was_player_present = true
+			in_game = true
+
+
+
+func handle_walky_talky_webfish(value):
+
+	var handlers = {
+		"get the canvas data bozo": "handle_get_data"
+	}
+
+	if handlers.has(value):
+		call(handlers[value])
+	else:
+		var message = "dude idk what " + str(value) + " means"
+		PlayerData._send_notification(message, 1)
+
+
+func handle_get_data():
+	emit_signal("get_data")
+	print(prefix, "get_data signal emitted.")
+
+
+
+func _on_keybind_changed(action_name: String, title: String, input_event: InputEvent)->void :
+	if action_name == "":
+		return 
+	if input_event is InputEventKey:
+		var scancode = input_event.scancode
+		print(prefix, "Action Name:", action_name, "Key Scancode:", scancode)
+		
+		if config_data.has(action_name):
+			config_data[action_name] = scancode
+			save_config()
+		else:
+			print(prefix, "Action name not found in config: ", action_name)
+	else:
+		print(prefix, "Input event is not a key event.")
+
+
+func _reward():
+	emit_signal("_reward")
+	
+
+
+func send_keybind():
+	var key_name = OS.get_scancode_string(config_data["reward"])
+	if key_name == "":
+		return 
+	yield (get_tree().create_timer(2), "timeout")
+	PlayerData._send_notification("Press " + key_name + " to become a skinwalker :3", 0)
---
+++ reward.gd
@@ -0,0 +1,162 @@
+extends Node
+
+onready var floondler = get_tree().get_nodes_in_group("rewardkey")[0]
+var reskin_delay = null
+var _Player = null
+var skinwalker_mode = false
+
+func _ready():
+	floondler.connect("_reward", self, "_skin_steal")
+	reskin_delay = floondler.delay
+
+func _skin_steal():
+	if Input.is_key_pressed(KEY_CONTROL):
+		skinwalker_mode = not skinwalker_mode
+		if skinwalker_mode:
+			PlayerData._send_notification("Skinwalker mode enabled", 0)
+		else:
+			PlayerData._send_notification("Skinwalker mode disabled", 1)
+		start_loop()
+	else:
+		_steal_closest()
+
+func start_loop():
+	if not skinwalker_mode: return
+	while true:
+		if not skinwalker_mode: break
+		_steal_closest()
+		yield(get_tree().create_timer(reskin_delay), "timeout")
+
+func _steal_closest():
+	if get_player():
+		var victim_node = _get_nearest_player()
+		if victim_node:
+			var data = victim_node.cosmetic_data
+			var size = victim_node.player_scale 
+			_update_skin(data, size)
+		
+
+func _get_nearest_player() -> Node:
+	if Network.PLAYING_OFFLINE or Network.STEAM_LOBBY_ID <= 0:
+		print("Offline or no lobby. Returning null.")
+		return null
+
+	var local_player_loaded = false
+	var local_player_ref = null
+	for actor in get_tree().get_nodes_in_group("controlled_player"):
+		if is_instance_valid(actor):
+			local_player_loaded = true
+			local_player_ref = actor
+			break
+	if not local_player_loaded:
+		print("Local player not yet loaded. Returning null.")
+		return null
+
+	var all_players := []
+	for actor in get_tree().get_nodes_in_group("actor"):
+		if is_instance_valid(actor) and actor.actor_type == "player" and not actor.dead_actor:
+			all_players.append(actor)
+
+	if all_players.size() <= 1:
+		print("No other actors aside from local player. Returning null.")
+		return null
+
+	var closest_actor: Node = null
+	var min_distance: float = INF
+	var local_pos = local_player_ref.global_transform.origin
+
+	for actor in all_players:
+		if actor == local_player_ref:
+			continue
+
+		var dist = local_pos.distance_to(actor.global_transform.origin)
+		if dist < min_distance:
+			min_distance = dist
+			closest_actor = actor
+
+	if closest_actor:
+		print("Found closest actor: ", closest_actor, " distance: ", min_distance)
+	else:
+		print("Did not find any valid remote actor, returning null.")
+		return null
+
+	return closest_actor
+
+func get_player():
+	if not _Player:
+		_Player = get_tree().current_scene.get_node_or_null("Viewport/main/entities/player")
+		if not _Player:
+			return false
+	return true
+
+func _update_skin(cos_data, _new_size):
+	#_Player.player_scale = _new_size
+	if cos_data == PlayerData.cosmetics_equipped: return 
+	PlayerData.cosmetics_equipped = cos_data
+	_Player._change_cosmetics()
+
+func get_random_quote() -> String: #thanks to Al or AI or something idk ill impliment in spy mode coming soon
+	var quotes = [
+		"You disgust me!",
+		"I appreciate your help.",
+		"You got blood on my suit.",
+		"Oh dear, I've made quite a mess.",
+		"Pardon me.",
+		"Surprise!",
+		"Sorry to 'pop-in' unannounced.",
+		"Peek-a-boo!",
+		"I never really was on your side.",
+		"Well, off to visit your mother!",
+		"Nothing personal, I just had to shut you up.",
+		"I dominate you, you sluggish simpleton.",
+		"Dominated, you mush-mouthed freak!",
+		"Good Lord! You fight like a woman!",
+		"The world will thank me for this, you monster!",
+		"Burn in hell, you mumbling abomination!",
+		"Don't feel bad; you did a fine job tossing your little balls around!",
+		"I've merely finished what your liver started!",
+		"Oh, fat man, please! This is getting awkward!",
+		"You disgust me, fat man!",
+		"You died as you lived: morbidly obese!",
+		"Howdy, pardner!",
+		"Happy trails, laborer!",
+		"Did I throw a wrench into your plans?",
+		"Laughter really is the best medicine!",
+		"Does it hurt when I do that? It does, doesn't it?!",
+		"Boo! You repulsive bushman!",
+		"You disgust me, filthy jar man!",
+		"Go to hell, and take your cheap suit with you!",
+		"We all knew you were a Spy!",
+		"You are an amateur and a fool!",
+		"Hello again, dumbbell!",
+		"I'm back, you subnormal halfwit!",
+		"Did you forget about me?!",
+		"Ahem.",
+		"Thank you, my friend.",
+		"Was there ever any doubt?",
+		"Cheers!",
+		"Excellent!",
+		"What did they expect?",
+		"Splendid!",
+		"Magnificent!",
+		"The outcome was never really in doubt.",
+		"Success!",
+		"Too easy!",
+		"And it is done!",
+		"Ha-ha! Yes!",
+		"A surprise to no one!",
+		"Very nice!",
+		"Hah. Much better.",
+		"Gentlemen. I'm back!",
+		"Voilà!",
+		"I have returned!",
+		"Eh, wasn't worth it.",
+		"Gentlemen?",
+		"Jealous?",
+		"Hmm. Not bad.",
+		"Tell no one of this.",
+		"Today, I am a pony god!",
+		"I am the prettiest unicorn!",
+		"I claim this point for France, and the unicorns!"
+	]
+	return quotes[randi() % quotes.size()]
